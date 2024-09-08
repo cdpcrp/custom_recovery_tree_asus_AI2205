76f00e43b62e2aa21dc65917b921ecb86ff28b68
Carlo Dee <carlodee.official@proton.me>
2024-09-08 04:57:13 +0000

    ASUS_AI2205: Removing Flash Current TWRP option. * For A/B devices that has dedicated recovery, it is unnecessary.
    Signed-off-by: Carlo Dee <carlodee.official@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -138,6 +138,7 @@ TW_DEFAULT_BRIGHTNESS := 200
 TW_OVERRIDE_SYSTEM_PROPS := \
     "ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
 TW_LOAD_VENDOR_MODULES := "adsp_loader_dlkm.ko rproc_qcom_common.ko qcom_q6v5.ko qcom_q6v5_pas.ko qcom_sysmon.ko twrp.ko"
+TW_NO_FLASH_CURRENT_TWRP := true
 
 # TWRP Debugging
 TARGET_USES_LOGD := true
