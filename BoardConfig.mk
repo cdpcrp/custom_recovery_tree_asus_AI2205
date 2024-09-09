e0f095398f034f9c2aca0946d2e57dd765acf425
Carlo Dee <carlodee.official@proton.me>
2024-09-09 13:11:23 +0000

    ASUS_AI2205: Set custom temperature path. * This is to help getting device temperature synced in TWRP.
    Signed-off-by: Carlo Dee <carlodee.official@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -141,6 +141,7 @@ TW_USE_SERIALNO_PROPERTY_FOR_DEVICE_ID := true
 TW_QCOM_ATS_OFFSET := 1666528204500
 TW_EXCLUDE_APEX := true
 TW_STATUS_ICONS_ALIGN := center
+TW_CUSTOM_CPU_TEMP_PATH := "/sys/devices/virtual/thermal/thermal_zone39/temp"
 TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
 TW_DEFAULT_BRIGHTNESS := 200
 TW_MAX_BRIGHTNESS := 1051
